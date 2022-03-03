9e2831c1f83a03033aafda2d39a25e5c2ac35248
Jhrnandez <juan@brandpetram.com>
2022-03-03 11:01:20 -0600

    ratio movil 3

--- components/BeforeFooter.js
+++ components/BeforeFooter.js
@@ -67,7 +67,7 @@ const BeforeFooter = () => {
               htmlAttributes={{ media: "(min-width: 320px)", alt:"Angel de la indpendencia ciudad de méxico" }}
               imgixParams={{
                 ar: "1:0.58",
-                h: "105",
+                h: "107",
                 fit: "crop",
                 auto: "compress",
               }}
