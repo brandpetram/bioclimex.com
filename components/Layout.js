2886fdeec741765fdcf1960e4db51499034df757
Jhrnandez <juan@brandpetram.com>
2022-04-06 13:06:42 -0500

    whatsapp

--- components/Layout.js
+++ components/Layout.js
@@ -14,6 +14,15 @@ const Layout = ({ children }) => {
       <div className="">
       {children}
       </div>
+      <div>
+        <a href="https://api.whatsapp.com/send/?phone=525520838489&text=Hola%2C+estoy+en+bioclimex.mx+y+me+interesa+cotizar">
+          <img
+            className="w-12 h-12 md:w-16 md:h-16 mb-3 mr-3 z-50 bottom-0 right-0 fixed"
+            src="/whatsapp.svg"
+            alt=""
+          />
+        </a>
+      </div>
       <FooterSocialLinksOnly />
     </div>
   );
