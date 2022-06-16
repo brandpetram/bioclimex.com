fd6837837f5775315c1cdc8426c1a5a05f4d4878
Jhrnandez <juan@brandpetram.com>
2022-06-16 14:48:00 -0500

    eventos de google ads

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import Layout from "../components/Layout";
 import Layout2 from "../components/Layout2";
 import Head from "next/head";
 import "../styles/embla.css"
+import Script from "next/script";
+import * as gtag from "../lib/gtag";
 
 const layouts = {
   L1: Layout,
@@ -27,6 +29,36 @@ function MyApp({ Component, pageProps }) {
 */}
   return (
       <Layout>
+          <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Script
+        strategy="lazyOnload"
+        id="gtag-manager"
+        dangerouslySetInnerHTML={{
+          __html: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-53HR246'); `,
+        }}
+      />
         <Head>
           <link
               rel="icon"
