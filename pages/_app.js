fe1f0ecad39e21d926f89cf9abb396943396e6ed
Juan Hernández <juan@brandpetram.com>
2023-05-29 19:07:53 +0000

    otro scrupt

--- pages/_app.js
+++ pages/_app.js
@@ -55,6 +55,27 @@ function MyApp({ Component, pageProps }) {
         `,
         }}
       />
+         <Script
+        strategy="lazyOnload"
+        id="clientify-2"
+        dangerouslySetInnerHTML={{
+          __html: ` if (typeof trackerCode ==='undefined'){
+            (function (d, w, u, o) {
+                w[o] = w[o] || function () {
+                    (w[o].q = w[o].q || []).push(arguments)
+                };
+                a = d.createElement('script'),
+                    m = d.getElementsByTagName('script')[0];
+                a.async = 1; a.src = u;
+                m.parentNode.insertBefore(a, m)
+            })(document, window, 'https://analytics.clientify.net/tracker.js', 'ana');
+            ana('setTrackerUrl', 'https://analytics.clientify.net');
+            ana('setTrackingCode', 'CF-28799-28799-RAVG0');
+            ana('trackPageview');
+        }
+        `,
+        }}
+      />
       <Script
         strategy="lazyOnload"
         id="gtag-manager"
