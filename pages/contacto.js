4ba256c297ba3add19259fdee3dee82033d0e563
Juan Manuel Henandez  Flores <juan@brandpetram.com>
2023-08-09 13:21:01 -0600

    formulario nuevo

--- pages/contacto.js
+++ pages/contacto.js
@@ -1,24 +1,48 @@
-import { Widget } from "@typeform/embed-react";
 import Head from "next/head";
-
-const Typeform = () => {
+import NextLink from "next/link";
+import Script from "next/script";
+const Contacto = () => {
   return (
-    <div className="">
-       <Head>
-          <title>Contacto | bioclimex</title>
-          <meta
-            name="description"
-            content="Los expertos en climatización de espacios industriales y comerciales en México"
-          />
-        </Head>
-      <Widget id="Z4uJvQhT" className="h-screen" />
-    </div>
+    <main className="bg-zinc-950">
+      <Head>
+        <title>Contacto | Bioclimex</title>
+      </Head>
+      <style jsx>{`
+        html {
+          margin: 0;
+          height: 100%;
+          overflow: hidden;
+        }
+        iframe {
+          position: absolute;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          top: 0;
+          border: 0;
+        }
+      `}</style>
+      <div className=" w-full h-full flex">
+        <iframe
+          src="https://apps.clientify.net/formbuilderembed/simpleembed/#/success/twostepformpopup/144/28799"
+          title="Contacto"
+          allow="camera;microphone"
+          className="flex w-full h-full"
+        ></iframe>
+        <Script
+          src="https://api.clientify.net/web-marketing/superforms/script/144689.js"
+          strategy="beforeInteractive"
+        ></Script>
+        <div className="z-50 absolute bottom-10 right-10 md:left-10 flex gap-0.2em font-light text-xs no-underline">
+          <NextLink href="/">
+            &larr;<span>Inicio</span>
+          </NextLink>
+        </div>
+      </div>
+    </main>
   );
 };
 
-Typeform.layout = "L2";
-{/* Solo es necesario poner el nombre del componente, seguido de .layout = "L2" si se requiere seleccionar el segundo layout (Layout2)
-    layout.js es el layout por defecto y no es necesario marcarlo explicitamente. 
-*/}
+Contacto.Layout = "L2";
 
-export default Typeform;
+export default Contacto;
