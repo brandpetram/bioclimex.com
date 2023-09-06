96bb6b89e9cdbb99e89af3cb8aa922e4a9f83220
Juan Manuel Henandez  Flores <juan@brandpetram.com>
2023-09-06 13:08:02 -0600

    typeform

--- pages/contacto.js
+++ pages/contacto.js
@@ -1,48 +1,24 @@
+import { Widget } from "@typeform/embed-react";
 import Head from "next/head";
-import NextLink from "next/link";
-import Script from "next/script";
-const Contacto = () => {
+
+const Typeform = () => {
   return (
-    <main className="bg-zinc-950">
+    <div className="">
       <Head>
-        <title>Contacto | Bioclimex</title>
+        <title>Contacto | bioclimex</title>
+        <meta
+          name="description"
+          content="Los expertos en climatización de espacios industriales y comerciales en México"
+        />
       </Head>
-      <style jsx>{`
-        html {
-          margin: 0;
-          height: 100%;
-          overflow: hidden;
-        }
-        iframe {
-          position: absolute;
-          left: 0;
-          right: 0;
-          bottom: 0;
-          top: 0;
-          border: 0;
-        }
-      `}</style>
-      <div className=" w-full h-full flex">
-        <iframe
-          src="https://apps.clientify.net/formbuilderembed/simpleembed/#/success/twostepformpopup/144/28799"
-          title="Contacto"
-          allow="camera;microphone"
-          className="flex w-full h-full"
-        ></iframe>
-        <Script
-          src="https://api.clientify.net/web-marketing/superforms/script/144689.js"
-          strategy="beforeInteractive"
-        ></Script>
-        <div className="z-50 absolute bottom-10 right-10 md:left-10 flex gap-0.2em font-light text-xs no-underline">
-          <NextLink href="/">
-            &larr;<span>Inicio</span>
-          </NextLink>
-        </div>
-      </div>
-    </main>
+      <Widget id="Z4uJvQhT" className="h-screen" />
+    </div>
   );
 };
 
-Contacto.Layout = "L2";
-
-export default Contacto;
+Typeform.layout = "L2";
+{
+  /* Solo es necesario poner el nombre del componente, seguido de .layout = "L2" si se requiere seleccionar el segundo layout (Layout2)
+    layout.js es el layout por defecto y no es necesario marcarlo explicitamente. 
+*/
+}
